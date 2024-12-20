d0b41690311e6c1e534bbd6adebdf8dcc77d585e
Aarush Ahuja <ahujaarush@gmail.com>
2024-12-20 16:38:37 +0530

    feat: add license

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from sigma.pipelines.qualys import qualys_pipeline
 
 backend = QualysBackend(processing_pipeline=qualys_pipeline())
 
-with open("rules/file_access.yml", "r") as f:
+with open("rule.yml", "r") as f:
     rule = SigmaRule.from_yaml(f.read())
     queries = backend.convert_rule(rule)
     print(f"Title: {rule.title}")
